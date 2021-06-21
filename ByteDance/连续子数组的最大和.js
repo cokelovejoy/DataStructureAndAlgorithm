14dd2cf8f66c09472ce68aa15f36eb026cdb32b8
cokelovejoy <847035485@qq.com>
2021-06-21 23:05:08 +0800

    bytedance

--- ByteDance/连续子数组的最大和.js
+++ ByteDance/连续子数组的最大和.js
@@ -1,2 +1,11 @@
 // 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
 // 要求时间复杂度为O(n)。
+
+var maxSubArray = function(nums) {
+    let len = nums.length;
+    let maxSum = nums[0];
+    for (let i = 0; i < len; i++) {
+
+
+    }
+};
