bcc7f3a837a824cc50c5cad9b3b862a26ac0b340
yq <847035485@qq.com>
2021-08-26 17:31:48 +0800

    js

--- js实现/js实现数组扁平化.js
+++ js实现/js实现数组扁平化.js
@@ -1,10 +1,10 @@
 // 数组扁平化flat
-let arr = [1,2,[3,4,[5,[6]]]]
-console.log(arr.flat(Infinity))//flat参数为指定要提取嵌套数组的结构深度，默认值为 1
+let arr = [1, 2, [3, 4, [5, [6]]]];
+console.log(arr.flat(Infinity)); //flat参数为指定要提取嵌套数组的结构深度，默认值为 1
 
 //用reduce实现
-function fn(arr){
-    return arr.reduce((prev,cur)=>{
-       return prev.concat(Array.isArray(cur)?fn(cur):cur)
-    },[])
- }
+function fn(arr) {
+  return arr.reduce((prev, cur) => {
+    return prev.concat(Array.isArray(cur) ? fn(cur) : cur);
+  }, []);
+}
